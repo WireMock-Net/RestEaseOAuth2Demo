7309a516655a2d822ff1cdb41c7cc2436740127b
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2017-05-25 00:01:30 +0200

    code

---
+++ src/RestEase.OAuth2.ConsoleTest/OAuthModel.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace RestEase.OAuth2.ConsoleTest
+{
+    public class OAuthModel
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+    }
+}
---
+++ src/RestEase.OAuth2/IMyApp.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using RestEase;
+
+namespace RestEaseOAuth2Test
+{
+    public interface IMyApp
+    {
+        [Get("helloworld")]
+        [Header("Authorization", "Bearer")]
+        Task<string> GetHelloAsync();
+    }
+}
